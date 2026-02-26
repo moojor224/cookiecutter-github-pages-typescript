043f5e58d118edf1824182e51df8fd16c2377a4b
Jordan Moore <moojor224@gmail.com>
2026-02-26 13:28:32 -0600

    Change template to vanilla TS
    Refactor page rendering to create and append a div with 'Hello World'.

---
+++ {{cookiecutter.project_slug}}/src/page.ts
@@ -0,0 +1,8 @@
+import "./page.css";
+
+// do something to the page here
+
+const root = document.getElementById("root");
+const div = document.createElement("div")
+div.textContent = "Hello World";
+root.append(div);
--- {{cookiecutter.project_slug}}/src/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./page.css";
-
-function Home() {
-    return <div></div>;
-}
-
-ReactDOM.createRoot(document.querySelector("#root")!).render(React.createElement(Home));
